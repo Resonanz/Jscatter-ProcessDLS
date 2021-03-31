678850331c78fb7764546b5313a03c3bf45760dd
Rob <rob.ward.nz@gmail.com>
2021-03-31 21:09:47 +1300

    Added Matplotlib and fixed laser lambda

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import jscatter as js
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 
 FILENAMES_TO_OPEN = [
     'Liposomes (30mW 1.5mL undiluted)'#,
@@ -20,7 +22,9 @@ G1_VS_GAMMA = ' (g1 vs gamma)'
 G1_VS_TAU = ' (g1 vs tau)'
 PRH_RH = ' (Prob Rh vs Rh)'
 
-NOISEY_LINES = 100  # Noisey lines of data at start of text file
+# If NAN appears in the Rh .csv, enlarge the value of NOISEY_LINES.
+# A value of 100 has been observed to produce NAN values.
+NOISEY_LINES = 150  # Noisey lines of data at start of text file
 
 search_text_start = '[CorrelationFunction]\n'
 search_text_end = '\n[RawCorrelationFunction]'
@@ -79,30 +83,23 @@ def write_cleaned_correlator_text_file(filename, text):
 This next block of code is mostly lifted from the Jscatter demo code DLS example.
 '''
 def do_contin_fitting(text):
-    t = js.loglist(1, 10000, 1000)  # times in microseconds
-    q = 4 * np.pi / 1.333 / 632 * np.sin(np.pi / 2)  # 90 degrees for 632 nm , unit is 1/nm**2
-    D = 0.05 * 1000  # nm**2/ns * 1000 = units nm**2/microseconds
-    gamma = q*q*D
-    print(np.sin(np.pi/2))
-    noise = 0.0001  # typical < 1e-3
-    data = js.dA(np.c_[t, 0.95 * np.exp(-q ** 2 * D * t) + noise * np.random.randn(len(t))].T)
-    #data = js.dA(FILENAME_PATH + filename_to_open + CLEANED + FILENAME_SIN_EXTENSION)
+    #t = js.loglist(1, 10000, 1000)  # times in microseconds
+    #q = 4 * np.pi / 1.333 / 632 * np.sin(np.pi / 2)  # 90 degrees for 632 nm , unit is 1/nm**2
+    #D = 0.05 * 1000  # nm**2/ns * 1000 = units nm**2/microseconds
+    #gamma = q*q*D
+    #print(np.sin(np.pi/2))
+    #noise = 0.0001  # typical < 1e-3
+    #data = js.dA(np.c_[t, 0.95 * np.exp(-q ** 2 * D * t) + noise * np.random.randn(len(t))].T)
+    data = js.dA(FILENAME_PATH + filename_to_open + CLEANED + FILENAME_SIN_EXTENSION)
     # add attributes to overwrite defaults
     data.Angle = 90  # scattering angle in degrees
     data.Temperature = 293  # Temperature of measurement  in K
     data.Viscosity = 1  # viscosity cPoise
     data.Refractive = 1.333  # refractive index
-    data.Wavelength = 632  # wavelength
+    data.Wavelength = 530  #632  # wavelength
     # do CONTIN
     dr = js.dls.contin(data, distribution='x')  # also use r
-    #bf = dr[0].contin_bestFit
-    #print(bf[3])
-    #print(dr.contin_bestFit[0].ipeaks)  # contains the 11 contin_bestFit.peaks results, but in three lists???
 
-    #print(dr[0].contin_bestFit)
-    #print(dr[0].contin_bestFit.ipeaks_name)
-
-    #js.dls.contin_display(dr)  # display overview
     return dr
 
 
@@ -144,7 +141,7 @@ def write_intensity_weight_plot_data(filename, Rh_data, prob_intensity_weight_da
     csv_to_write = 'Rh, P(Rh) Intensity Weight, P(Rh) Mass Weight, P(Rh) Number Weight, \n'
 
     for r in range(len(Rh_data)):
-        csv_to_write += str(Rh_data[r])
+        csv_to_write += str(math.log10(Rh_data[r]))
         csv_to_write += ','
         csv_to_write += str(prob_intensity_weight_data[r])
         csv_to_write += ','
@@ -179,3 +176,24 @@ if __name__ == '__main__':
         print(dr[0].contin_bestFit.ipeaks)
         print(dr[0].contin_bestFit.ipeaks[0,2]/dr[0].contin_bestFit.ipeaks[0,1])
 
+        # generate random data for plotting
+        x = np.linspace(0.0,100,20)
+
+        # now there's 3 sets of points
+        y1 = np.random.normal(scale=0.2,size=20)
+        y2 = np.random.normal(scale=0.5,size=20)
+        y3 = np.random.normal(scale=0.8,size=20)
+
+        # plot the 3 sets
+        plt.plot(x,y1,label='plot 1')
+        plt.plot(x,y2, label='plot 2')
+        plt.plot(x,y3, label='plot 3')
+
+        plt.xlabel("Living Area Above Ground")
+        plt.ylabel("House Price")
+
+        # call with no parameters
+        plt.legend()
+
+        plt.savefig(FILENAME_PATH + filename_to_open + PRH_RH + ".pdf")
+        plt.show()
